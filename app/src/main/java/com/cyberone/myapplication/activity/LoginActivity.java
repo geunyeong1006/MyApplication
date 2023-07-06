034756b7c0162d808b82b0cc02b907dfc7e1524b
cyberone <geunyeong.jeong@cyberone.kr>
2023-07-06 18:41:33 +0900

    DB연결진행중

--- app/src/main/java/com/cyberone/myapplication/activity/LoginActivity.java
+++ app/src/main/java/com/cyberone/myapplication/activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.cyberone.myapplication.activity;
 
 import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,12 +9,28 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cyberone.myapplication.R;
+import com.cyberone.myapplication.repository.MySQLConnectionManager;
+
+import java.sql.Connection;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        try {
+//            Connection connection = MySQLConnectionManager.getConnection();
+//
+//            // 연결된 데이터베이스를 사용하여 작업 수행
+//
+//            connection.close(); // 작업 완료 후 연결 닫기
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } catch (java.sql.SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+
 
         Button signUpButton = findViewById(R.id.buttonSignUp);
         signUpButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/cyberone/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/cyberone/myapplication/activity/MainActivity.java
@@ -2,15 +2,45 @@ package com.cyberone.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.cyberone.myapplication.R;
+import com.cyberone.myapplication.repository.MySQLConnectionManager;
+
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            MySQLConnectionManager.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        resultTextView = findViewById(R.id.resultTextView);
+
+        // AsyncTask를 사용하여 백그라운드에서 쿼리 실행 및 결과 처리
+        new QueryTask().execute();
+    }
+
+    private class QueryTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            String query = "SELECT * FROM users";
+            return MySQLConnectionManager.executeQuery(query);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            resultTextView.setText(result);
+        }
     }
 }
